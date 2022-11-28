c9a3bd50d97c4e01b4f621bd0d47295c96e40402
Nanda-Souza <fesouoli@gmail.com>
2022-11-27 21:17:23 -0300

    Layout 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div class="container-pagina">
+
+     <div class="container-superior">
+
+      <div class="forca">
+        <img src="assets/forca0.png" alt=""></img>
+      </div>
+
+      <div class="botao">
+        <button class="escolha">Escolher Palavra</button>
+      </div>
+
+     </div>
+
+     <div class="container-letras">
+
+     <div class="botao-letras">
+        <button class="letras">A</button>
+
+      </div>
+      <div class="botao-letras">
+        <button class="letras">B</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">C</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">D</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">E</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">F</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">G</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">H</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">I</button>
+      </div>
+
+      <div class="botao-letras">
+        <button class="letras">J</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">K</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">L</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">M</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">N</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">O</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">P</button>
+      </div>
+
+      <div class="botao-letras">
+        <button class="letras">Q</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">R</button>
+      </div>
+
+      <div class="botao-letras">
+        <button class="letras">S</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">T</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">U</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">V</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">W</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">X</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">Y</button>
+      </div>
+      
+      <div class="botao-letras">
+        <button class="letras">Z</button>
+      </div>
+
+      </div>
+
+    <div class="container-inferior">
+      
+      <div class="chute">
+        <p class="chute-palavra">Já sei a palavra!</p>
+        <input type="text" class="texto" placeholder="" />
+        <button class="chutar">Chutar</button>
+      </div>
+      </div>
+
+     
+    </div>   
+  )
 }
 
-export default App;
+export default App
+
